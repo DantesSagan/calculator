42d8ef603da1e68a94464e16d0a27ed4422397fd
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-23 18:28:11 +0500

    3d commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import NumbersNlogic from './components/numbers';
 import React from 'react';
 
-export default function App() {
-  const [recording, setRecording] = React.useState('');
- 
+// const numbers = [
+//   {
+//     id: 'one',
+//     value: 1,
+//   },
+//   {
+//     id: 'two',
+//     value: 2,
+//   },
+//   {
+//     id: 'three',
+//     value: 3,
+//   },
+//   {
+//     id: 'four',
+//     value: 4,
+//   },
+//   {
+//     id: 'five',
+//     value: 5,
+//   },
+//   {
+//     id: 'six',
+//     value: 6,
+//   },
+//   {
+//     id: 'seven',
+//     value: 7,
+//   },
+//   {
+//     id: 'eight',
+//     value: 8,
+//   },
+//   {
+//     id: 'nine',
+//     value: 9,
+//   },
+//   {
+//     id: 'zero',
+//     value: 0,
+//   },
+// ];
+
+export default function App({ op, display, formula, action, func }) {
+  // const [recording, setRecording] = React.useState('');
+
   return (
     <div className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden d-flex p-4 mt-44'>
-      <div className='output' id='display'>
-        Output {recording}
+      <div
+        className='p-4'
+        id='formula'
+      ></div>
+      <div
+        className='p-4'
+        id='formula2'
+      ></div>
+      <div className='p-4' id='display'>
+        Result:
       </div>
-      <NumbersNlogic  setRecording={setRecording} />
+      {/* Output {recording} */}
+      {/* {numbers.map((numberObj) => ( */}
+      <NumbersNlogic display={display} action={action} formula={formula} />
+      {/* // setRecording={setRecording} */}
+      {/* ))} */}
     </div>
   );
 }
--- src/components/numbers.js
+++ src/components/numbers.js
@@ -1,85 +1,255 @@
-export default function NumbersNlogic({ op, result, setRecording }) {
+import React from 'react';
+
+export default function NumbersNlogic({ action, formula, formula2 }) {
   const func = () => {
-    var result;
-    var num1 = (document.getElementById('one').value);
-    var num2 = (document.getElementById('two').value);
-    switch (op) {
+    var display;
+    var num1 = Number(document.getElementById('formula').value);
+    var num2 = Number(document.getElementById('formula2').value);
+    switch (action) {
+      case '0':
+        formula = 0;
+        formula2 = 0;
+        break;
+      case '1':
+        formula = 1;
+        formula2 = 1;
+        break;
+      case '2':
+        formula = 2;
+        formula2 = 2;
+        break;
+      case '3':
+        formula = 3;
+        formula2 = 3;
+        break;
+      case '4':
+        formula = 4;
+        formula2 = 4;
+        break;
+      case '5':
+        formula = 5;
+        formula2 = 5;
+        break;
+      case '6':
+        formula = 6;
+        formula2 = 6;
+        break;
+      case '7':
+        formula = 7;
+        formula2 = 7;
+        break;
+      case '8':
+        formula = 8;
+        formula2 = 8;
+        break;
+      case '9':
+        formula = 9;
+        formula2 = 9;
+        break;
       case '+':
-        result = num1 + num2;
+        formula2 = '+';
+        display = num1 + num2;
         break;
       case '-':
-        result = num1 - num2;
+        display = num1 - num2;
         break;
       case '*':
-        result = num1 * num2;
+        display = num1 * num2;
         break;
       case '/':
         if (num2) {
-          result = num1 / num2;
+          display = num1 / num2;
         } else {
-          result = 'бесконечность';
+          display = 'бесконечность';
         }
         break;
+      case '%':
+        display = (num1 / 100) * num2;
+        break;
+      case 'sqrt':
+        display = Math.sqrt(num1);
+        break;
+      case 'decimal':
+        formula = '.';
+        break;
+      case 'clear':
+        formula = 0;
+        formula2 = 0;
+        display = 0;
+        break;
       default:
-        result = 'Введите данные&&Выберите операцию';
+        display = 'Введите данные&&Выберите операцию';
     }
-    document.getElementById('result').innerHTML = result;
+    document.getElementById('display').innerHTML = display;
+    document.getElementById('formula').innerHTML = formula;
+    document.getElementById('formula2').innerHTML = formula2;
   };
+  var number2 = 1234.56789; // floating point example
+  number2.toLocaleString(undefined, { maximumFractionDigits: 2 }); // "1,234.57"
   return (
     <div className='grid grid-cols-2 gap-4'>
       <div className='grid grid-cols-3 gap-4'>
-        <button className='col-span-2' id='clear' value='AC'>
+        <button
+          className='col-span-2'
+          id='clear'
+          value='AC'
+          onClick={() => func((action = 'clear'))}
+        >
           AC
         </button>
-
-        <button id='one' value='1'>
+        <button
+          className=''
+          id='one'
+          value='1'
+          onClick={() => func((action = '1'))}
+        >
           1
         </button>
-        <button id='two' value='2'>
+        <button
+          className=''
+          id='two'
+          value='2'
+          onClick={() => func((action = '2'))}
+        >
           2
         </button>
-        <button id='three' value='3'>
+        <button
+          className=''
+          id='three'
+          value='3'
+          onClick={() => func((action = '3'))}
+        >
           3
         </button>
-        <button id='four' value='4'>
+        <button
+          className=''
+          id='four'
+          value='4'
+          onClick={() => func((action = '4'))}
+        >
           4
         </button>
-        <button id='five' value='5'>
+        <button
+          className=''
+          id='five'
+          value='5'
+          onClick={() => func((action = '5'))}
+        >
           5
         </button>
-        <button id='six' value='6'>
+        <button
+          className=''
+          id='six'
+          value='6'
+          onClick={() => func((action = '6'))}
+        >
           6
         </button>
-        <button id='seven' value='7'>
+        <button
+          className=''
+          id='seven'
+          value='7'
+          onClick={() => func((action = '7'))}
+        >
           7
         </button>
-        <button id='eight' value='8'>
+        <button
+          className=''
+          id='eight'
+          value='8'
+          onClick={() => func((action = '8'))}
+        >
           8
         </button>
-        <button id='nine' value='9'>
+        <button
+          className=''
+          id='nine'
+          value='9'
+          onClick={() => func((action = '9'))}
+        >
           9
         </button>
-        <button className='' id='zero' value='0'>
+        <button
+          className=''
+          id='zero'
+          value='0'
+          onClick={() => func((action = '0'))}
+        >
           0
         </button>
-        <button className='' id='decimal' value='.' onClick={(op = ' . ')}>
+        <button
+          className=''
+          id='zerozero'
+          value='00'
+          onClick={() => func((action = '00'))}
+        >
+          00
+        </button>
+        <button
+          className=''
+          id='decimal'
+          value='.'
+          onClick={() => func((action = '.'))}
+        >
           .
         </button>
       </div>
+
       <div className='grid grid-col-1 gap-4'>
-        <button className='' id='divide' value='/' onClick={(op = ' / ')}>
+        <button
+          className=''
+          id='percentage'
+          value='%'
+          onClick={() => (action = '%')}
+        >
+          %
+        </button>
+        <button
+          className=''
+          id='sqrt'
+          value='sqrt'
+          onClick={() => (action = 'sqrt')}
+        >
+          \sqrt
+        </button>
+        <button
+          className=''
+          id='divide'
+          value='/'
+          onClick={() => (action = '/')}
+        >
           /
         </button>
-        <button className='...' id='multiply' value='*' onClick={(op = ' * ')}>
+        <button
+          className='...'
+          id='multiply'
+          value='*'
+          onClick={() => (action = '*')}
+        >
           *
         </button>
-        <button className='...' id='subtract' value='-' onClick={(op = ' - ')}>
+        <button
+          className='...'
+          id='subtract'
+          value='-'
+          onClick={() => (action = '-')}
+        >
           -
         </button>
-        <button className='...' id='add' value='+' onClick={(op = ' + ')}>
+        <button
+          className='...'
+          id='add'
+          value='+'
+          onClick={() => (action = '+')}
+        >
           +
         </button>
-        <button className='...' id='equals' value='=' onClick={func()}>
+        <button
+          className='...'
+          id='equals'
+          value='='
+          onClick={() => func((action = '='))}
+        >
           =
         </button>
       </div>
